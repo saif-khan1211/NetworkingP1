c49474fbbef2a22e5c6a231fef0a68ea8e2a5363
saif-khan1211 <59232614+saif-khan1211@users.noreply.github.com>
2022-10-29 22:55:06 -0400

    Update saifsFile.py

--- saifsFile.py
+++ saifsFile.py
@@ -1,41 +1,114 @@
-from unittest.mock import NonCallableMagicMock
-import uuid
+import binascii
+from logging import exception
 import socket
 import sys
+import time
+from xmlrpc import server
+
+def createDNSQuery(hostName):
+    dnsQuery = {}
+
+    #prepare the header
+
+    #Revelevent Fields
+    dnsQuery['ID'] = 'AAAA'
+    dnsQuery['queryParams'] = '0100'
+    dnsQuery['RD'] = '0001'
+    #dnsQuery['QDCOUNT'] = '0000000000000001'
+    dnsQuery['QDCOUNT'] = '000000000000'
 
 
-UDP_IP = '8.8.8.8'
-MAX_ATTEMPTS = 3
+    header = dnsQuery['ID'] + dnsQuery['queryParams'] + dnsQuery['RD'] + dnsQuery['QDCOUNT']
+
+    #prepare the question
+
+    question = ""
+
+    hostNameArray = hostName.split()
+    
+    website = hostNameArray[1].split('.')
+
+    #Get QNAME
+    qName = ''
+    for i in range(len(website)):
+        qName += str(len(website[i]))
+        print(qName)
+        for j in range(len(website[i])):
+            qName += str(format(ord(website[i][j]), "x"))
+    
+    #Get QTYPE
+    #qType = '1'
+    qType = '0000010001'
 
 
-def sendQuery(dns_query, address, port):
-    print("in send Query")
+    #question = qName + qType
+    question = qName
 
-#Populates DNS query when creating it
-def populateDNSQuery(dns_query):
-    uuidFour = uuid.uuid4()
-    dns_query["id"] = hex(int(str(uuidFour).split("-")[1], 16))
-    dns_query["QR"] = hex(int("0x0", 16))
-    dns_query["OPCODE"] = hex(int("0x0", 16))
-    dns_query["RD"] = hex(int("0x1", 16))
-    dns_query["QDCOUNT"] = hex(int("0x1", 16))
-    #dns_query["QNAME"] = 
-    #dns_query["QTYPE"] = 
+    query = header + question
 
-#Host name provided by the user. 
-
-def main():
-    userInput = input("Enter Host name: ")
-    print(userInput)
+    print('DNS Header = ' + header)
+    print('DNS Question = ' + question)
+    print('DNS Query = ' + query) 
 
 
-    if userInput == '':
-        print("Please input an argument")
-        exit()
+def sendQuery(query):
+    query = 'AAAA01000001000000000000036564750000010001'
 
-    # Setting up DNS query. Only has header and question, need to prepare a message
+    print(query)
+    response = ""
+    serverAddress = ("8.8.8.8", 53)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    count = 1
+    print("Going into Loop!!!!")
+    while count < 4: 
+        print("IN THE LOOOOOP")
+        try:
+            print('Attempt - ' + str(count))
+            sock.sendto(binascii.unhexlify(query), serverAddress)
+            sock.settimeout(5)
+            response, _ = sock.recvfrom(1024)
 
-    dns_query = {}
-    populateDNSQuery(dns_query)
+            if response != "":
+                print("Successfull")
+                break
 
-main()
+        
+        except Exception as e:
+            print("Failed creating a connection " + str(e))
+            count += 1
+
+
+
+    sock.close()  
+    if count >= 4:
+        print("Unable to send Query.")
+        sys.exit()
+    
+    print('Query response retreived')
+    return binascii.hexlify(response).decode("utf-8")
+
+    
+
+def recieveAndProcessResponse(responseQuery):
+    print("In recieve and ResponseQuery")
+
+def main(hostName):
+
+    #print('-----HELLO, WELCOME TO DNS CLIENT-----')
+    #print('-------Please Enter a Host Name-------')
+    query = createDNSQuery(hostName)
+    #print('--------------------------------------')
+    #print('-------------Sending Query------------')
+    responseQuery = sendQuery(query)
+   # print('------------Recieving Query-----------')
+    #recievedQuery = recieveAndProcessResponse(responseQuery)
+
+    # print('END OF PROGRAM, THANK YOU!')
+
+
+if __name__ == "__main__":
+    #hostName = sys.argv[1]
+    #  my−dns−client gmu.edu
+    #hostName = input('Enter hostName ')
+    hostName = "my−dns−client gmu.edu"
+    main(hostName)
